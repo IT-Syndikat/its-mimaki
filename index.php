30e168341e88993107203da99948a77c43f4b38d
Tyrolyean <tyrolyean@gmx.at>
2018-05-11 23:58:14 +0200

    Apparently a check is a checkbox
    i should learn html a bit better

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 		<p>Plot things!</p>
 		<form method="POST" action="upload.php" enctype="multipart/form-data">
                         <input type="file" id="print" name="print" accept="*.hpgl" required></input>
-                        <input type="check" id="scale" name="scale"></input>
+                        <input type="checkbox" id="scale" name="scale"></input>
 			<button type="submit">PRINT</button>
 		</form>
 	</body>
